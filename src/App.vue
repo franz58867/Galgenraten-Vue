82dc81f11bbffad99671119155fc9a26301c131a
Franz Stallbohm <franz.stallbohm@telefonica.com>
2026-09-03 13:51:55 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { reactive } from "Vue";
+
+const gameState = reactive({
+  word: "",
+  mistakes: 0,
+  guessedLetters: [],
+  gameLetters: [
+    "a", "b", "c", "d", "e", "f", "g",
+    "h", "i", "j", "k", "l", "m", "n",
+    "o", "p", "q", "r", "s", "t", "u",
+    "v", "w", "x", "y", "z",
+    "ä", "ö", "ü", "ß"
+  ]
+})
+
+function guessLetter(letter) {
+  console.log(letter);
+}
+
+</script>
+
+<template>
+  <button v-for="letter in gameState.gameLetters" :key="letter" @click="guessLetter(letter)">
+    {{ letter }}
+  </button>
+</template>
